26036ab2cfc4184aef323b17a9f6430d4461bc5a
Anthony Villegas <anthonyvillegas@Anthonys-MacBook-Pro.local>
2024-08-01 16:55:20 -0700

    python dict final

--- python-dictionaries/dictionary-exercise.py
+++ python-dictionaries/dictionary-exercise.py
@@ -1,7 +1,15 @@
 def making_dict(n):
     binary_dict = {}
     for num in n:
+        bin_dict_str = ''
+        temp_remain = num
         while num > 0:
             remainder = num % 2
-            
-        
+            bin_dict_str = str(remainder) + bin_dict_str
+            temp_remain //=2
+        if len(bin_dict_str) <4:
+            bin_dict_str = '0'* (4-len(bin_dict_str)) +bin_dict_str
+        binary_dict[num] = bin_dict_str
+    return binary_dict
+
+print(making_dict(15))
