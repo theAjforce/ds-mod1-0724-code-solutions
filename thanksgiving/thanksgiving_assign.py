49b114aa9d11a4546ae7bec1c6b7e2666d7d19dd
Anthony Villegas <anthonyvillegas@Anthonys-MBP.lan>
2024-08-02 13:26:30 -0700

    thanksgiving assignemnt final

--- thanksgiving/thanksgiving_assign.py
+++ thanksgiving/thanksgiving_assign.py
@@ -4,12 +4,8 @@ def days_until_thanksgiving():
     today = dt.date.today()
     year = today.year
     november = dt.date(year, 11, 1)
-    
-    # Find the first Thursday of November
     days_to_first_thursday = (3 - november.weekday()) % 7
     first_thursday = november + dt.timedelta(days=days_to_first_thursday)
-    
-    # Thanksgiving is the fourth Thursday
     thanksgiving = first_thursday + dt.timedelta(weeks=3)
     
     return (thanksgiving - today).days
