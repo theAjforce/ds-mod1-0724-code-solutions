c05442b6c82fc145b1eaeedfb3a5c9fc2964db2a
Anthony Villegas <anthonyvillegas@Anthonys-MacBook-Pro.local>
2024-08-05 16:56:58 -0700

    incomplete dates

--- custom-datetimes/datetimes.py
+++ custom-datetimes/datetimes.py
@@ -7,10 +7,14 @@ def date_parser(date):
         elif date[-5] == "/":
             dt = datetime.strptime(date, "%d/%m/%Y")
         else:
-            return "Need assistance parsing" #I included this clause because after a bit of research I was unable to find how to parse the ones with the timezones included, and I forgot what advice was given to Newton.
+            date_split = date.split()
+            timezone = date_split.pop()
+            dt = date_split
+            
     elif "T" in date:
         dt = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f%z")
     else:
         date.isdigit()
         dt = datetime.fromtimestamp(int(date))
     return dt
+print(date_parser("March 23rd, 2021 13:01 US/Pacific"))
