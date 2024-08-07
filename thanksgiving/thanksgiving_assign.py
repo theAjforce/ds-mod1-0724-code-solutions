eeb9fdfbe9271972be6c9f15bae835994a7f2bce
Anthony Villegas <anthonyvillegas@Anthonys-MacBook-Pro.local>
2024-08-07 16:14:03 -0700

    thanksgiving fixed

--- thanksgiving/thanksgiving_assign.py
+++ thanksgiving/thanksgiving_assign.py
@@ -3,10 +3,9 @@ import datetime as dt
 def days_until_thanksgiving():
     today = dt.date.today()
     year = today.year
-    november = dt.date(year, 11, 1)
-    days_to_first_thursday = (3 - november.weekday()) % 7
-    first_thursday = november + dt.timedelta(days=days_to_first_thursday)
-    thanksgiving = first_thursday + dt.timedelta(weeks=3)
+    november = dt.date(year, 11, 30)
+    days_to_last_thursday = (november.weekday() - 3) % 7
+    thanksgiving = november - dt.timedelta(days=days_to_last_thursday)
     
     return (thanksgiving - today).days
 print(days_until_thanksgiving())
